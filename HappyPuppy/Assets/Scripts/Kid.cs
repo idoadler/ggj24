8c2a4d19766b2289cb7c3c905a62ede1dbda7f35
ido <games@ido.wtf>
2024-01-26 14:17:26 +0800

    kid runs away

--- HappyPuppy/Assets/Scripts/Kid.cs
+++ HappyPuppy/Assets/Scripts/Kid.cs
@@ -1,12 +1,13 @@
-using NavMeshPlus.Extensions;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Kid : MonoBehaviour
 {
     public GameObject dog;
+    public GameObject[] absoluteDirs;
 
     private NavMeshAgent _agent;
+    private GameObject _lastTarget;
     
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,47 @@ public class Kid : MonoBehaviour
     void Update()
     {
         Vector3 dirToPlayer = transform.position - dog.transform.position;
-        _agent.SetDestination(dog.transform.position);
+
+        Vector3 maxPoint = Vector3.zero;
+        GameObject maxObject = null;
+        var run_away = transform.position + dirToPlayer;
+        _agent.SetDestination(run_away);
+        return;
+        if (PointInRoom(run_away))
+        {
+            maxPoint = run_away;
+            maxObject = dog;
+        }
+        else
+        {
+            float maxDist = 100000;
+            foreach (var dir in absoluteDirs)
+            {
+                var pos = dir.transform.position;
+                var newDist = Vector3.Distance(pos, dog.transform.position);
+                if (maxDist > newDist)
+                {
+                    maxDist = newDist;
+                    maxPoint = pos;
+                    maxObject = dir;
+                }
+            }
+        }
+
+        if (maxObject != _lastTarget)
+        {
+            _lastTarget = maxObject;
+            print("Target: "+_lastTarget.name);
+        }
+        _agent.SetDestination(maxPoint);
+    }
+    
+    bool PointInRoom(Vector2 point)
+    {
+        const float range = 1f;
+        NavMeshHit hit;
+        var res = NavMesh.SamplePosition(point, out hit, range, NavMesh.AllAreas);
+        print("hit("+point+"):" +res + "=" + hit.position);
+        return res;
     }
 }
