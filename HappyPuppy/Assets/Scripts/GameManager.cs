796c3fffbef8198c1be71092bf419dc7254d9267
ido <games@ido.wtf>
2024-01-27 08:52:18 +0800

    make movement a bit more playful

--- HappyPuppy/Assets/Scripts/GameManager.cs
+++ HappyPuppy/Assets/Scripts/GameManager.cs
@@ -3,11 +3,18 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    private const float WAIT_TIME = 1;
+    
     public int happyPoints;
     public TMP_Text scoreText;
 
+    private float _lastScore;
+
     public void IncreaseOfHappiness()
     {
+        if(Time.time - _lastScore < WAIT_TIME)
+            return;
+        _lastScore = Time.time;
         happyPoints += 1;
         scoreText.text = "Score: " + happyPoints;
     }
--- HappyPuppy/Assets/Scripts/Kid.cs
+++ HappyPuppy/Assets/Scripts/Kid.cs
@@ -5,10 +5,12 @@ public class Kid : MonoBehaviour
 {
     const int DIR_NUM = 8;
     const float STEP_LEN = 10;
+    const float TARGET_DIST = 2;
 
     public GameObject dog;
 
     private NavMeshAgent _agent;
+    private Vector3 _target;
     
     // Start is called before the first frame update
     void Start()
@@ -16,32 +18,18 @@ public class Kid : MonoBehaviour
         _agent = GetComponent<NavMeshAgent>();
         _agent.updateRotation = false;
         _agent.updateUpAxis = false;
-    }
-
-    void OnDrawGizmosSelected()
-    {
-        Vector3 dirToPlayer = transform.position - dog.transform.position;
-        var r = transform.position + Quaternion.AngleAxis(0, Vector3.up) * dirToPlayer.normalized*STEP_LEN;
-        Gizmos.color = Color.green;
-        Gizmos.DrawLine(transform.position, r);
-        if (PointInRoom(r)) return;
-        for (int i = 1; i < DIR_NUM; i++)
-        {
-            var run_away = transform.position + Quaternion.AngleAxis(180f/DIR_NUM*i, Vector3.forward) * dirToPlayer.normalized*STEP_LEN;
-            Gizmos.color = PointInRoom(run_away) ? Color.blue : Color.red;
-            Gizmos.DrawLine(transform.position, run_away);
-
-            run_away = transform.position + Quaternion.AngleAxis(-180f/DIR_NUM*i, Vector3.forward) * dirToPlayer.normalized*STEP_LEN;
-            Gizmos.color = PointInRoom(run_away) ? Color.cyan : Color.magenta;
-            Gizmos.DrawLine(transform.position, run_away);
-
-        }
+        _target = RunAwayPosition();
+        _agent.SetDestination(_target);
     }
     
     // Update is called once per frame
     void Update()
     {
-        _agent.SetDestination(RunAwayPosition());
+        if (Vector2.Distance(transform.position, _target) < TARGET_DIST)
+        {
+            _target = RunAwayPosition();
+            _agent.SetDestination(_target);
+        }
     }
 
     Vector3 RunAwayPosition()
@@ -64,5 +52,25 @@ public class Kid : MonoBehaviour
     bool PointInRoom(Vector2 point)
     {
         return NavMesh.SamplePosition(point, out _, 1, NavMesh.AllAreas);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Vector3 dirToPlayer = transform.position - dog.transform.position;
+        var r = transform.position + Quaternion.AngleAxis(0, Vector3.up) * dirToPlayer.normalized*STEP_LEN;
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, r);
+        if (PointInRoom(r)) return;
+        for (int i = 1; i < DIR_NUM; i++)
+        {
+            var run_away = transform.position + Quaternion.AngleAxis(180f/DIR_NUM*i, Vector3.forward) * dirToPlayer.normalized*STEP_LEN;
+            Gizmos.color = PointInRoom(run_away) ? Color.blue : Color.red;
+            Gizmos.DrawLine(transform.position, run_away);
+
+            run_away = transform.position + Quaternion.AngleAxis(-180f/DIR_NUM*i, Vector3.forward) * dirToPlayer.normalized*STEP_LEN;
+            Gizmos.color = PointInRoom(run_away) ? Color.cyan : Color.magenta;
+            Gizmos.DrawLine(transform.position, run_away);
+
+        }
     }
 }
